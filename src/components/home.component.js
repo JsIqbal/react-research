08316c52bd9702234cb4371230f71750eff9ab65
JsIqbal <javascriptiqbal@gmail.com>
2022-11-19 00:48:12 +0600

    Heroku Fake Data Test

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,7 +1,44 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function Home () {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function getUsers () {
+            try {
+                const data = await axios.get("https://fakedata69.herokuapp.com/data");
+
+                setUsers(data);
+            }
+            catch(err) {
+                console.log(err);
+                alert("Error Happened In Fetching Data");
+            }   
+        }
+        getUsers();
+    }, []);
+
+    console.log(users.data);
     return(
-        <h1>This is home</h1>
+        <>
+            <h1>This are fake-data:</h1>
+
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>username</th>
+                </tr>
+                <tr>
+                    {
+                        users.data.map((item) => {
+                            return <td>{item.id}</td>
+                        })
+                    }
+                </tr>
+            </table>
+        </>
     );
 }
